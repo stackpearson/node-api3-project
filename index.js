0f9a96f158391ed6255e6d21925e132544fb09a9
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-09-07 22:09:16 -0600

    logger middleware complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const server = require('./server.js');
+
 // code away!
+// const server = require('./server.js');
+// const logger = require('./server.js');
+
+// server.use(logger())
+
+// server.listen(4000, () => {
+//     console.log('\n* Server Running on http://localhost:4000 *\n');
+//   });
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const userRouter = require('./users/userRouter');
+const helmet = require ('helmet');
 
 const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,15 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const time = new Date().toISOString();
+  console.log(`Method: ${req.method} | endpoint: ${req.url} | time: ${time} |`)
+  next();
+}
+
+server.listen(4000, () => {
+  console.log('\n* Server Running on http://localhost:4000 *\n');
+});
 
 module.exports = server;
+
